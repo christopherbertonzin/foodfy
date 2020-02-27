d07b3e2015d96217e4668bf46484921523bf93ba
Christopher Moreto Bertonzin <>
2020-02-27 17:52:09 -0300

    finalizando modal

---
+++ README.md
@@ -0,0 +1,12 @@
+# Foodfy
+
+> Site de receitas 
+
+## Tecnologias utilizadas
+
+- Html
+- CSS
+- JavaScript
+
+## 
+<img src="./assets/gif.gif">
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,17 @@ const modalOverlay = document.querySelector('.modalOverlay')
 
 for (let card of cards) {
     card.addEventListener('click', () => {
+        const id = card.getAttribute('id')
+        
+        const description = card.querySelector('p').textContent
+
+        const title = card.querySelector('h2').textContent
+
         modalOverlay.classList.add('active')
+        
+        modalOverlay.querySelector('img').src = `./assets/img/${id}.png`
+        modalOverlay.querySelector('h1').textContent = `${title}`
+        modalOverlay.querySelector('p').textContent = `${description}`
         
     })
 }

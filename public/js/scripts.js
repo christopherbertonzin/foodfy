1b2a4b8f3873fa5924b5a936bf263d84c7ce0b8d
christopherbertonzin <bertonzin@outlook.com>
2020-03-20 00:05:45 -0300

    update: repipe

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,10 +1,20 @@
 const cards = document.querySelectorAll('.card')
-const modalOverlay = document.querySelector('.modalOverlay')
+// const buttons = document.querySelectorAll('#btn--display')
+// const recipeContent = document.querySelectorAll('.recipe--content')
+
+
 
 for (let card of cards) {
     card.addEventListener('click', () => {
-        window.location('')
+        const id = card.id
+        window.location.href=`/recipe?id=${id}`
         
     })
 }
 
+// for (let button in buttons) {
+//     button.addEventListener('click', () => {
+//         console.log('ok')
+//         // recipeContent.class.add('recipe--active')
+//     })
+// }

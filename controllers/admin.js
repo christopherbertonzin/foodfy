6d4e497268385a96dfed280778d4e15511aabe20
christopherbertonzin <bertonzin@outlook.com>
2020-06-16 22:30:54 -0300

    feat: complete admin page

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,122 @@
 const fs = require('fs')
 const data = require('../data.json')
+const { render } = require('nunjucks')
 
-exports.index = (req, res) => {}
-exports.create = (req, res) => {}
-exports.show = (req, res) => {}
-exports.edit = (req, res) => {}
-exports.post = (req, res) => {}
-exports.put = (req, res) => {}
-exports.delete = (req, res) => {}
+exports.index = (req, res) => {
+    return res.render('admin/index', { items: data.recipes})
+}
+
+exports.create = (req, res) => {
+    return res.render('admin/create')
+}
+
+exports.post = (req, res) => {
+    const keys = Object.keys(req.body)
+
+    for (let key of keys) {
+        if (req.body[key] == '') {
+            res.send('Preencha todos os campos!')
+        }
+    }
+
+    req.body.id = Number(data.recipes.length + 1)
+    req.body.author = `por ${req.body.author}`
+
+    const { id, author, title, image, ingredients, preparation, information } = req.body
+
+    data.recipes.push({
+        id,
+        author,
+        title,
+        image,
+        ingredients,
+        preparation,
+        information
+    })
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 4), (err) => {
+        if (err) return res.send('Falha ao salvar dados no servidor')
+
+        return res.redirect('/admin/recipes')
+    } )
+
+}
+
+exports.show = (req, res) => {
+    const { id } = req.params
+
+    const foundRecipe = data.recipes.find((recipe) => {
+        if (recipe.id == id) {
+            return true
+        }
+    })
+    
+    if (!foundRecipe) {
+        return res.render('not-found')
+    }
+    return res.render('admin/recipe', { item: foundRecipe})
+}
+
+exports.edit = (req, res) => {
+    const { id } = req.params
+
+    const foundRecipe = data.recipes.find((recipe) => {
+        if (recipe.id == id) {
+            return true
+        }
+    })
+    
+    if (!foundRecipe) {
+        return res.render('not-found')
+    }
+    return res.render('admin/edit', { recipe: foundRecipe})
+}
+
+exports.put = (req, res) => {
+    const { id } = req.body
+    let index = 0
+
+    const foundRecipe = data.recipes.find((recipe, indexRecipe) => {
+        if (recipe.id == id) {
+            index = indexRecipe
+            return true
+        }
+    })
+
+    if (!foundRecipe) {
+        return res.send('Receita não encontrada')
+    }
+
+    const recipe = {
+        ...foundRecipe,
+        ...req.body,
+        id: Number(foundRecipe.id)
+    }
+
+    data.recipes[index] =  recipe
+
+    console.log(recipe)
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 4), (err) => {
+        if (err) return res.send('Erro ao salvar dados')
+
+        return res.redirect(`/admin/recipes/${id}`)
+    })
+
+
+}
+exports.delete = (req, res) => {
+    const { id } = req.body
+
+    const foundRecipe = data.recipes.filter((recipe) => {
+        return id != recipe.id
+    })
+
+    data.recipes = foundRecipe
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 4), (err) => {
+        if (err) return res.send('Erro ao salvar dados')
+
+        return res.redirect(`/admin/recipes/`)
+    })
+}
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const methodOverride = require('method-override')
 const routes = require('./routes')
 const server = express()
 
 server.use(express.static('public'))
+server.use(express.urlencoded({ extended:true }))
+server.use(methodOverride('_method'))
 server.set('view engine', 'njk')
-nunjucks.configure('views', { express: server, noCache: true})
+nunjucks.configure('views', { 
+    express: server,
+    autoescape: false,
+    noCache: true})
+
 server.use(routes)
 
 routes.use((req,res) => {
@@ -14,4 +21,4 @@ routes.use((req,res) => {
 
 
 // STARTING SERVER
-server.listen('3000')
+server.listen('5000')
